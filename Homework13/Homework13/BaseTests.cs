1b8411300a94b0c387ba2377c73e948f9991a884
Maksim Drebchev <maksim.drebchev@grafixoft.com>
2022-07-05 00:00:45 +0300

    Tests for Homework13

---
+++ Homework13/Homework13/BaseTests.cs
@@ -0,0 +1,34 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework13
+{
+    public abstract class BaseTests
+    {
+        protected abstract string Url { get; }
+
+        protected IWebDriver Driver { get; set; }
+
+        [SetUp]
+        public void Setup()
+        {
+            ChromeOptions chromeOptions = new ChromeOptions();
+            chromeOptions.AddArgument("start-maximized");
+            Driver = new ChromeDriver(chromeOptions);
+            Driver.Navigate().GoToUrl(Url);
+            //Driver.Manage().Window.Maximize();
+        }
+
+        [TearDown]
+        public void Quit()
+        {
+            Driver.Quit();
+        }
+    }
+}
